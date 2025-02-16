f84853b23a3d5aa55a8ff769f257aad893a667e3
bhavik patel <bhavikpatel773241@gmail.com>
2025-02-16 18:10:35 +0530

    vercel error commit

--- vite.config.ts
+++ vite.config.ts
@@ -4,11 +4,16 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist'
+  },
+  base: '/',
   server: {
     headers: {
       "Cross-Origin-Embedder-Policy": "require-corp",
       "Cross-Origin-Opener-Policy": "same-origin"
     },
+
     // proxy: {
     //   '/cdn': {
     //     target: 'http://localhost:3006',
